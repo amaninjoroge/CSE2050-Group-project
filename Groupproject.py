9be806644403a287e077624e50966e7bf3fe54db
amaninjoroge <amaninjoroge18@gmail.com>
2026-02-24 17:30:21 -0500

    added  class Course, as well as methods add_student and get_student_count

--- Groupproject.py
+++ Groupproject.py
@@ -1,2 +1,12 @@
-class Course:
-    
+class Course: #Amani
+    def __init__(self, course_code, credits, students):
+        """function representing a single course in the university catalog"""
+        self.course_code = str(course_code)
+        self.credits = int(credits)
+        self.students = []
+    def add_student(self, student):
+       self.student = student
+       self.student.append(student)
+    def get_student_count(self):
+        return len(self.students)
+
