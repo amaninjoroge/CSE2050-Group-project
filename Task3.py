28c2d8eab24837e0a854bb64b85a12d08b9122fc
amaninjoroge <amaninjoroge18@gmail.com>
2026-02-24 17:47:38 -0500

    Create Task3.py

---
+++ Task3.py
@@ -0,0 +1,14 @@
+class University:
+    def __init__(self):
+      pass  
+    def add_course(self, course_code, credits):
+       self.course_code = course_code
+       self.credits = credits
+    pass
+    def add_student(self, student_id, name):
+       self.student_id = student_id
+       self.name = name 
+    pass
+    def get_student(self):
+       
+    
