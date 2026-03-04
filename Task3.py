ec93ecee77f98de8991d60807106a45033ce324a
amaninjoroge <amaninjoroge18@gmail.com>
2026-03-04 11:36:25 -0500

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,14 +1,32 @@
-class University:
+class University: #Amani
     def __init__(self):
-      pass  
+        self.students = {}
+        self.courses = {}
+
     def add_course(self, course_code, credits):
-       self.course_code = course_code
-       self.credits = credits
-    pass
+        if course_code not in self.courses:
+            self.courses[course_code] = Course(course_code, credits)
+        return self.courses[course_code]
+
     def add_student(self, student_id, name):
-       self.student_id = student_id
-       self.name = name 
-    pass
-    def get_student(self):
-       
-    
+        if student_id not in self.students:
+            self.students[student_id] = Student(student_id, name)
+        return self.students[student_id]
+
+    def get_student(self, student_id):
+        return self.students.get(student_id)
+
+    def get_course(self, course_code):
+        return self.courses.get(course_code)
+
+    def get_course_enrollment(self, course_code):
+        course = self.get_course(course_code)
+        if course is None:
+            return 0
+        return len(course.students)
+
+    def get_students_in_course(self, course_code):
+        course = self.get_course(course_code)
+        if course is None:
+            return []
+        return list(course.students)
