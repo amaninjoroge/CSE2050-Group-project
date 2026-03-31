f6195ffa68120a3c5d88a7c44f6e891b55c6fa86
amaninjoroge <amaninjoroge18@gmail.com>
2026-03-30 20:20:49 -0400

    Update Groupproject.py

--- Groupproject.py
+++ Groupproject.py
@@ -299,5 +299,49 @@ if __name__ == "__main__": #Amani (demo)
     for name in common:
         print(name)
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.head = None  
+        self.tail = None   
+        self.size = 0
+
+    def enqueue(self, item):
+        new_node = Node(item)
+
+        if self.tail is None:      
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.size += 1
+
+    def dequeue(self):
+        if self.head is None:
+            raise ValueError("Queue is empty")
+
+        value = self.head.data
+        self.head = self.head.next
+
+        if self.head is None:      
+            self.tail = None
+
+        self.size -= 1
+        return value
+
+    def is_empty(self):
+        return self.size == 0
+
+    def __len__(self):
+        return self.size
+    
+
+
+
             
 
